e4f285fd8e169fe9de6909fbece3852a7b84969e
Егор Шкарин <schkarin.egor@yandex.ru>
2021-01-15 13:02:02 +0300

    Создана модель пользователя

---
+++ CodeBlogFitnessBL/Model/Gender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodeBlogFitnessBL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название пола.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Создание пола.
+        /// </summary>
+        /// <param name="name">Имя пола</param>
+        public Gender(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя поле не может быть пустым или null", nameof(name));
+            }
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
